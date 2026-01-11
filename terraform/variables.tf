aa8d67204ed6ed1a748ab7adf552fac99faf6126
democloudarun <democloud@rediffmail.com>
2026-01-12 01:14:04 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,10 +2,9 @@ variable "azure_subscription_id" {}
 variable "azure_client_id" {}
 variable "azure_client_secret" {}
 variable "azure_tenant_id" {}
-variable "ssh_public_key" {}
-
 variable "ssh_public_key" {
   description = "SSH public key for VM access"
   type        = string
 }
+
 
