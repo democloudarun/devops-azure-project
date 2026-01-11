294b6f8351f3df44ea293809fe96baaa5a283c52
democloudarun <democloud@rediffmail.com>
2026-01-12 00:38:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -67,4 +72,5 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
 
